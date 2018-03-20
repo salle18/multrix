86bfff41c7f1d25fe3af0865c5cb084fb06de320
salle18 <salle18@gmail.com>
2018-03-20 15:48:23 +0100

    Make sure to cast port to string when running server

--- src/clj/multrix/server.clj
+++ src/clj/multrix/server.clj
@@ -5,5 +5,5 @@
   (:gen-class))
 
  (defn -main [& args]
-   (let [port (Integer/parseInt (or (env :port) "3000"))]
+   (let [port (Integer/parseInt (or (str (env :port)) "3000"))]
      (run-jetty app {:port port :join? false})))

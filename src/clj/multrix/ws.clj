6854e8155ca396fcc14b1e8c2921a511741ea5bd
salle18 <salle18@gmail.com>
2018-03-22 13:44:30 +0100

    Added open and close connection handlers

--- src/clj/multrix/ws.clj
+++ src/clj/multrix/ws.clj
@@ -20,6 +20,8 @@
   (def chsk-send! send-fn)
   (def connected-uids connected-uids))
 
+(def ping-counts (atom 0))
+
 (defmulti -event-msg-handler
   "Multimethod to handle Sente `event-msg`s"
   :id)
@@ -36,6 +38,24 @@
     (->output! "Unhandled event: %s" event)
     (when ?reply-fn
       (?reply-fn {:umatched-event-as-echoed-from-from-server event}))))
+
+(defmethod -event-msg-handler :chsk/ws-ping
+  [_]
+  (let [c (swap! ping-counts inc)]
+    (when (zero? (mod c 10001)) (reset! ping-counts 0))
+    (when (zero? (mod c 10))
+      (->output! "Ping counts: %s" c))))
+
+(defmethod -event-msg-handler :chsk/uidport-open
+  [{:as ev-msg :keys [event id ?data ring-req ?reply-fn client-id send-fn]}]
+  (let [session (:session ring-req)]
+    (->output! "Connected: %s" client-id)))
+
+(defmethod -event-msg-handler :chsk/uidport-close
+  [{:as ev-msg :keys [event id ?data ring-req ?reply-fn client-id send-fn]}]
+  (let [session (:session ring-req)
+        uid     (:uid session)]
+    (->output! "Disconnected: %s" client-id)))
 
 (defonce router_ (atom nil))
 

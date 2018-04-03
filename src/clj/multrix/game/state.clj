2c3ee27c428520bb507b364482589bfbed7b6c2e
salle18 <salle18@gmail.com>
2018-04-03 23:43:59 +0200

    Add logic for updating next block and losing game.

--- src/clj/multrix/game/state.clj
+++ src/clj/multrix/game/state.clj
@@ -2,7 +2,9 @@
   (:require [multrix.game.config :refer [max-number-of-clients board-width board-height]]
             [multrix.util.seq :as seq]
             [multrix.game.blocks :as blocks]
+            [multrix.game.status :as status]
             [multrix.game.direction :as direction]
+            [multrix.util.log :as log]
             [multrix.game.fields :as fields]))
 
 (def new-game-state
@@ -14,11 +16,15 @@
 (def new-game-board
   (vec (repeat board-height (vec (repeat board-width fields/empty-field)))))
 
+(defn new-bag []
+  (vec (shuffle (flatten (map #(repeat 4 %) blocks/tetrominos)))))
+
 (def new-client-state
-  {:board      new-game-board
-   :bag        blocks/tetrominos-bag
-   :block      nil
-   :next-block nil})
+  {:player-status status/playing
+   :board         new-game-board
+   :bag           (new-bag)
+   :block         nil
+   :next-block    nil})
 
 (defn init-block [block]
   {:type      block
@@ -31,7 +37,9 @@
     (assoc client-state :bag (next (next bag)) :block (init-block (first bag)) :next-block (second bag))))
 
 (defn update-in-client-state [client-uid f]
-  (swap! game-state$ update-in [:client-states client-uid] f))
+  (swap! game-state$ update-in [:client-states client-uid]
+         (fn [{:as client-state :keys [player-status]}]
+           (if (= player-status status/playing) (f client-state) client-state))))
 
 (defn map-block [f {:keys [type direction x y]}]
   (let [position (get-in type [:positions direction])]
@@ -52,13 +60,27 @@
            (fn [x y] (and (<= 0 x board-width) (<= y board-height) (empty-field? board x y)))
            block)))
 
+(defn merge-block-board [block board]
+  (let [indices (map-block (fn [x, y] [y x]) block)]
+    (reduce #(assoc-in %1 %2 fields/full-field) board indices)))
+
+(defn update-next-block [{:as client-state :keys [board bag next-block block]}]
+  (let [new-block (init-block next-block)]
+    (if (allowed-block? board new-block)
+      (-> client-state (assoc :block new-block)
+          (assoc :next-block (first bag))
+          (assoc :bag (or (next bag) (new-bag)))
+          (assoc :board (merge-block-board block board)))
+      (-> client-state (assoc :player-status status/lost) (assoc :board (merge-block-board new-block board))))))
+
 (defn move [client-uid move-direction]
   (let [client-state (get-in @game-state$ [:client-states client-uid])
         block        (:block client-state)
         board        (:board client-state)]
     (condp = move-direction
       direction/down (if (allowed-block? board (update block :y inc))
-                       (update-in-client-state client-uid (fn [client-state] (update-in client-state [:block :y] inc))))
+                       (update-in-client-state client-uid (fn [client-state] (update-in client-state [:block :y] inc)))
+                       (update-in-client-state client-uid update-next-block))
       direction/left
       (if (allowed-block? board (update block :x dec))
         (update-in-client-state client-uid (fn [client-state] (update-in client-state [:block :x] dec))))
@@ -95,14 +117,11 @@
   (let [{:keys [client-uids client-states]} @game-state$]
     (map #(pack-clients-state (get client-states %)) client-uids)))
 
-(defn merge-block-board [{:keys [block board]}]
-  (let [indices (map-block (fn [x, y] [y x]) block)]
-    (reduce #(assoc-in %1 %2 fields/full-field) board indices)))
-
 (defn client-state [client-uid]
-  (let [{:keys [client-states]} @game-state$
-        client-state            (get client-states client-uid)]
-    {:board (merge-block-board client-state)}))
+  (let [{:keys [client-states]}                        @game-state$
+        {:keys [board block player-status]}            (get client-states client-uid)]
+    {:board         (merge-block-board block board)
+     :player-status player-status}))
 
 (defn- add-client-state [{:keys [client-uids client-states]} client-uid]
   (let [empty-index (.indexOf client-uids nil)]
